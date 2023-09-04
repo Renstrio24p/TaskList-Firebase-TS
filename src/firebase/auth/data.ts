230ae264db5cd84dc0b0bc3ccb5a2dc181d84fa3
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-09-05 00:37:53 +0800

    remove the actual data

--- src/firebase/auth/data.ts
+++ src/firebase/auth/data.ts
@@ -1,14 +1,9 @@
 
-export const apiKey = "AIzaSyBloCLSiJHJ2Ay0auHE_dFEwb031uQe2ak",
-
-  authDomain = "tasklist-7be77.firebaseapp.com",
-
-  projectId = "tasklist-7be77",
-
-  storageBucket = "tasklist-7be77.appspot.com",
-
-  messagingSenderId = "600907379036",
-
-  appId = "1:600907379036:web:e4487ffb6325b3d029d199",
-
-  measurementId = "G-3K7G6D3J9X"
+export const
+  apiKey = "",
+  authDomain = "",
+  projectId = "",
+  storageBucket = "",
+  messagingSenderId = "",
+  appId = "",
+  measurementId = ""
